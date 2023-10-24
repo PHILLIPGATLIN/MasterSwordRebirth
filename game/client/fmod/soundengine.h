909db9cc65c378130571fce1f15b79e5862a158e
Saint Wish <SaintWish@users.noreply.github.com>
2023-10-23 19:11:04 -0600

    add terminology

--- game/client/fmod/soundengine.h
+++ game/client/fmod/soundengine.h
@@ -5,6 +5,21 @@
 #include <unordered_map>
 #include <fmod/fmod.hpp>
 
+/* Notes on terminology: 
+As far as fmod is concerned, a "sound" corresponds to a file on disk, but a "channel" is what you 
+actually use to play the sound, pause, adjust the volume, etc. We only want to load a sound once, 
+but we might want to play the sound multiple times simultaneously. Therefore we need to create sounds
+and channels separately and treat them as distinct things, as you may have multiple channels linked to
+a single sound. In short, sound = file and channel = what gets played.
+
+A "group" is a collection of sounds. It is useful because a group can have its own properties that are 
+then mixed with the properties of the sounds in its collection. The most obvious example is volume;
+if a group has a volume property of 0.5f and a sound in that group has its own volume property of
+0.75f, the final result is the sound will play at 0.375f volume (0.5f*0.75f=0.375f). In practice,
+this implementation uses two groups that correspond to Half-Life's "SFX Volume" and "MP3 Volume"
+sliders in the audio settings menu.
+*/
+
 class CSoundEngine
 {
 public:
